4465673019793180c4fb5a8f2e34606d84a8e328
Syed Abdullah <syed_khoab@outlook.com>
2022-11-08 17:50:24 +0600

    Updated and fixed some issues.

--- MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -8,10 +8,6 @@ import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
 
-    private lateinit var userNameData:TextView
-    private lateinit var emailData:TextView
-    private lateinit var addressData:TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -20,13 +16,14 @@ class SecondActivity : AppCompatActivity() {
         val email=intent.getStringExtra("email")
         val address=intent.getStringExtra("address")
 
-        val userNametv=findViewById<TextView>(R.id.userNameDataView)
+        val userNameTv=findViewById<TextView>(R.id.userNameDataView)
         val emailTv=findViewById<TextView>(R.id.emailDataView)
         val addressTv=findViewById<TextView>(R.id.addressDataView)
 
-        userNametv.text="User Name: "+userName
-        emailTv.text="Email: "+email
-        addressTv.text="Address: "+address
+        userNameTv.text= "User Name: $userName"
+        emailTv.text= "Email: $email"
+        addressTv.text = "Address: $address"
+
 
 
         val btn=findViewById<Button>(R.id.backButton)
