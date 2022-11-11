0a4afae7be09a7307ea7b7f9a1b79e90a9c7677c
Syed Abdullah <syed.abdullah@bjitacademy.com>
2022-11-11 15:38:58 +0600

    Dice Roller app updated

--- DiceRoller/app/src/main/java/com/syedabdullah/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/syedabdullah/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.syedabdullah.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         // this event handler will update the dice value on the textView
         rollButton.setOnClickListener{
             val dice =rollDice()
+            imageView(dice)
             checkResult(dice)
         }
     }
@@ -30,24 +32,39 @@ class MainActivity : AppCompatActivity() {
         return diceRoll
     }
 
-    //checking result if the user get the lucky number
-    private fun checkResult(dice:Int){
-        val resultView:TextView=findViewById(R.id.resultTextView)
-        var lucky=luckyNumber(6)
-        if(dice==lucky){
-            resultView.text="Congratulation! You won."
-        }
-        else{
-            resultView.text="Sorry! lucky number was $lucky"
+    //set dice value to Image view
+    private fun imageView(dice:Int){
+        val diceImageView:ImageView=findViewById(R.id.diceImageView)
+        when(dice){
+            1->diceImageView.setImageResource(R.drawable.dice_1)
+            2->diceImageView.setImageResource(R.drawable.dice_2)
+            3->diceImageView.setImageResource(R.drawable.dice_3)
+            4->diceImageView.setImageResource(R.drawable.dice_4)
+            5->diceImageView.setImageResource(R.drawable.dice_5)
+            else->diceImageView.setImageResource(R.drawable.dice_6)
         }
     }
 
-    //return a lucky number
+
+    //checking result if the user get the lucky Dice
+    private fun checkResult(dice:Int){
+        val resultView:TextView=findViewById(R.id.resultTextView)
+        val luckyNum=luckyNumber(6)
+        if(dice==luckyNum){
+            "Congratulation! You won.".also { resultView.text = it }
+        }
+        else{
+            "Sorry! lucky dice was $luckyNum".also { resultView.text = it }
+        }
+    }
+
+    //return a lucky Dice
     private fun luckyNumber(numSides:Int):Int{
-        return (1..6).random()
+        return (1..numSides).random()
     }
 }
 
+// Define a Class for Dice
 class Dice(private val numSides:Int){
     //return a random number from 1 to numSides
     fun roll():Int{
