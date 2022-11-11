11783dabbb96b465754041941304ee9a18ac7a44
Syed Abdullah <syed.abdullah@bjitacademy.com>
2022-11-11 11:33:06 +0600

    Some minor changes in Roll Dice App

--- DiceRoller/app/src/main/java/com/syedabdullah/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/syedabdullah/diceroller/MainActivity.kt
@@ -15,17 +15,36 @@ class MainActivity : AppCompatActivity() {
 
         // this event handler will update the dice value on the textView
         rollButton.setOnClickListener{
-            rollDice()
+            val dice =rollDice()
+            checkResult(dice)
         }
     }
 
     //update the dice with value 1 to 6 and show a toast that the dice is rolled!
-    private fun rollDice() {
+    private fun rollDice() : Int{
         val dice=Dice(6)
         val diceRoll=dice.roll()
         val diceTextView:TextView=findViewById(R.id.diceTextView)
         diceTextView.text=diceRoll.toString()
         Toast.makeText(this,"Dice Rolled!", Toast.LENGTH_SHORT).show()
+        return diceRoll
+    }
+
+    //checking result if the user get the lucky number
+    private fun checkResult(dice:Int){
+        val resultView:TextView=findViewById(R.id.resultTextView)
+        var lucky=luckyNumber(6)
+        if(dice==lucky){
+            resultView.text="Congratulation! You won."
+        }
+        else{
+            resultView.text="Sorry! lucky number was $lucky"
+        }
+    }
+
+    //return a lucky number
+    private fun luckyNumber(numSides:Int):Int{
+        return (1..6).random()
     }
 }
 
