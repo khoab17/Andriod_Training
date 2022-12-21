0f91328693c36409e5ee5b54f86686be0d4e98ec
Syed Abdullah <syed_khoab@outlook.com>
2022-12-21 22:36:33 +0600

    fragment application codelab point 6 covered.

---
+++ android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.wordsapp
+
+class Constants {
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+}
---
+++ android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,46 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentWordListBinding
+
+class WordListFragment : Fragment() {
+    private var _binding:FragmentWordListBinding?=null
+    private val binding get()=_binding!!
+    private lateinit var recyclerView:RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding=FragmentWordListBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView=binding.recyclerView
+        recyclerView.layoutManager=LinearLayoutManager(requireContext())
+        recyclerView.adapter=WordAdapter(activity?.intent?.extras?.getString(Constants.LETTER).toString(), requireContext())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context,DividerItemDecoration.VERTICAL)
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
+    }
+}
