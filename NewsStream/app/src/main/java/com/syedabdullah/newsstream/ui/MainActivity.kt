4e23e90270295089251091fbfae58953e963d9e5
Syed Abdullah <syed_khoab@outlook.com>
2023-01-16 22:47:15 +0600

    some update news project

---
+++ NewsStream/app/src/main/java/com/syedabdullah/newsstream/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.syedabdullah.newsstream.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.syedabdullah.newsstream.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private var _binding:ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragmentContainer.id) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
---
+++ NewsStream/app/src/main/java/com/syedabdullah/newsstream/ui/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.syedabdullah.newsstream.ui.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.syedabdullah.newsstream.ui.HomeFragment
+
+private const val NUMS_TAB = 3
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle):
+    FragmentStateAdapter(fragmentManager, lifecycle)
+{
+    override fun getItemCount(): Int {
+        return NUMS_TAB
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0->HomeFragment ()
+            else->HomeFragment()
+        }
+    }
+
+}
